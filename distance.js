0387cab39ffa3e858a8e60a87cb9c4f666405396
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-05-23 11:00:10 -0400

    final revision

--- distance.js
+++ distance.js
@@ -4,4 +4,4 @@ let y1 = 7;
 let y2 = 9;
 
 
-console.log("The distance between the two quordinates is " + ((x2-x1)**2 + ((y2-y1)**2))**2 + " inchs.");
+console.log("The distance between the two quordinates is " + Math.sqrt(((x2-x1)**2 + ((y2-y1)**2))) + " inchs.");
